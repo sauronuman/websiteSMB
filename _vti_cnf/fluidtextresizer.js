a0609a3a5b9f1fde8f10554bc950deacafc5defd
D Gartland <dgartland021@c2ken.net>
2024-02-24 19:38:39 +0000

    Initial commit of website files

---
+++ _vti_cnf/fluidtextresizer.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|20 Nov 2023 14:17:50 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|C2KEN\\smcburney933
+vti_modifiedby:SR|C2KEN\\smcburney933
+vti_timecreated:TR|20 Nov 2023 14:17:50 -0000
+vti_cacheddtm:TX|20 Nov 2023 14:17:50 -0000
+vti_filesize:IR|3948
+vti_backlinkinfo:VX|
